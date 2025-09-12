0c26d68f56de6ea4b5822ddac5f689ee8c9e3968
Starknet Dev <starknetdev@gmail.com>
2025-09-12 15:00:41 +0100

    - update addresses - update indexer - update countdown

--- client/src/contexts/Statistics.tsx
+++ client/src/contexts/Statistics.tsx
@@ -2,7 +2,13 @@ import { getPriceChart, getSwapQuote } from "@/api/ekubo";
 import { useStarknetApi } from "@/api/starknet";
 import { useGameTokens } from "@/dojo/useGameTokens";
 import { NETWORKS } from "@/utils/networkConfig";
-import { createContext, PropsWithChildren, useContext, useEffect, useState } from 'react';
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 export interface StatisticsContext {
   gamePrice: string | null;
@@ -15,16 +21,22 @@ export interface StatisticsContext {
 }
 
 // Create a context
-const StatisticsContext = createContext<StatisticsContext>({} as StatisticsContext);
+const StatisticsContext = createContext<StatisticsContext>(
+  {} as StatisticsContext
+);
 
-export const OPENING_TIME = 1757586362;
+export const OPENING_TIME = 1757694600;
 export const totalSurvivorTokens = 2000000;
 export const totalCollectableBeasts = 93150;
 export const JACKPOT_AMOUNT = 33333;
 
 const DungeonTicket = NETWORKS.SN_MAIN.dungeonTicket;
-const STRK = NETWORKS.SN_MAIN.paymentTokens.find(token => token.name === 'STRK')?.address!;
-const USDC = NETWORKS.SN_MAIN.paymentTokens.find(token => token.name === 'USDC')?.address!;
+const STRK = NETWORKS.SN_MAIN.paymentTokens.find(
+  (token) => token.name === "STRK"
+)?.address!;
+const USDC = NETWORKS.SN_MAIN.paymentTokens.find(
+  (token) => token.name === "USDC"
+)?.address!;
 
 // Create a provider component
 export const StatisticsProvider = ({ children }: PropsWithChildren) => {
@@ -33,33 +45,37 @@ export const StatisticsProvider = ({ children }: PropsWithChildren) => {
 
   const [gamePrice, setGamePrice] = useState<string | null>(null);
   const [gamePriceHistory, setGamePriceHistory] = useState<any[]>([]);
-  const [remainingSurvivorTokens, setRemainingSurvivorTokens] = useState<number | null>(null);
+  const [remainingSurvivorTokens, setRemainingSurvivorTokens] = useState<
+    number | null
+  >(null);
   const [collectedBeasts, setCollectedBeasts] = useState(0);
   const [strkPrice, setStrkPrice] = useState<string | null>(null);
 
   const fetchCollectedBeasts = async () => {
     const result = await countBeasts();
-    setCollectedBeasts((result - 75) || 0);
+    setCollectedBeasts(result - 75 || 0);
   };
 
   const fetchPriceHistory = async () => {
     const strkPrice = await getPriceChart(DungeonTicket, STRK);
     setGamePriceHistory(strkPrice.data);
-  }
+  };
 
   const fetchGamePrice = async () => {
     const swap = await getSwapQuote(-1e18, DungeonTicket, USDC);
-    setGamePrice((swap.total * -1 / 1e6).toFixed(2));
-  }
+    setGamePrice(((swap.total * -1) / 1e6).toFixed(2));
+  };
 
   const fetchStrkPrice = async () => {
     const swap = await getSwapQuote(100e18, STRK, USDC);
     setStrkPrice((swap.total / 1e6 / 100).toFixed(2));
-  }
+  };
 
   const fetchRewardTokensClaimed = async () => {
     const result = await getRewardTokensClaimed();
-    setRemainingSurvivorTokens(result !== null ? totalSurvivorTokens - result : null);
+    setRemainingSurvivorTokens(
+      result !== null ? totalSurvivorTokens - result : null
+    );
   };
 
   useEffect(() => {
@@ -71,14 +87,16 @@ export const StatisticsProvider = ({ children }: PropsWithChildren) => {
   }, []);
 
   return (
-    <StatisticsContext.Provider value={{
-      gamePrice,
-      gamePriceHistory,
-      strkPrice,
-      fetchRewardTokensClaimed,
-      remainingSurvivorTokens,
-      collectedBeasts,
-    }}>
+    <StatisticsContext.Provider
+      value={{
+        gamePrice,
+        gamePriceHistory,
+        strkPrice,
+        fetchRewardTokensClaimed,
+        remainingSurvivorTokens,
+        collectedBeasts,
+      }}
+    >
       {children}
     </StatisticsContext.Provider>
   );
@@ -87,8 +105,7 @@ export const StatisticsProvider = ({ children }: PropsWithChildren) => {
 export const useStatistics = () => {
   const context = useContext(StatisticsContext);
   if (!context) {
-    throw new Error('useStatistics must be used within a StatisticsProvider');
+    throw new Error("useStatistics must be used within a StatisticsProvider");
   }
   return context;
 };
-
