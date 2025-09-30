2c49a45d9a722466e061ed37692df6cb1f7c162b
loothero <loothero@provable.games>
2025-09-30 03:37:03 -0700

    update dungeon ticket address

--- client/src/utils/networkConfig.ts
+++ client/src/utils/networkConfig.ts
@@ -60,7 +60,7 @@ export const NETWORKS = {
     dungeon:
       "0x010eedffdca8123d95e21025f5166465d5539c8f575d2369fd31abe31ca17186",
     dungeonTicket:
-      "0x04e9b7794d3f4affaca03d4e88d35db205a5a2325b0ed2b3889c93f0867858eb",
+      "0x0466f84e54796fe9f9b0c6b921f8f10af3b9dc32303cb49211d54fcb1987be5b",
     beasts:
       "0x046da8955829adf2bda310099a0063451923f02e648cf25a1203aac6335cf0e4",
     goldenToken:
@@ -96,7 +96,7 @@ export const NETWORKS = {
       {
         name: "TICKET",
         address:
-          "0x04e9b7794d3f4affaca03d4e88d35db205a5a2325b0ed2b3889c93f0867858eb",
+          "0x0466f84e54796fe9f9b0c6b921f8f10af3b9dc32303cb49211d54fcb1987be5b",
         displayDecimals: 0,
       },
       {
